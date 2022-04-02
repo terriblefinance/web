467102036a9312d414e4ae91843eae54b6ffb299
Terence Ponce <terence@terrible.finance>
2022-04-02 16:34:50 +0800

    Install Material UI and add basic layout
    This adds Material UI into the project. This also adds a basic layout
    for the app's home page.
    
    I really tried to use Tailwind CSS but I'm too stupid to figure
    it out because I was already bad with plain CSS in the first place.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
-import "../styles/globals.css";
+import type { ReactElement } from "react";
 import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import type { Page } from "../src/types/page";
+import createEmotionCache from "../src/utilities/createEmotionCache";
+import lightThemeOptions from "../src/theme/lightThemeOptions";
+
+import "../styles/globals.css";
+
+type AppPropsWithLayout = AppProps & {
+  Component: Page;
+  emotionCache?: EmotionCache;
+};
+
+const clientSideEmotionCache = createEmotionCache();
+const lightTheme = createTheme(lightThemeOptions);
+
+const MyApp = ({ Component, pageProps, emotionCache = clientSideEmotionCache }: AppPropsWithLayout) => {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
 
 export default MyApp;
